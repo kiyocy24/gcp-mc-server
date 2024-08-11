7a4c548103d70bcf686c76d310794b2482394d9e
kiyocy24 <kiyocy24@gmail.com>
2024-08-11 17:17:17 +0900

    terrafomr.tfvars

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,24 +1,27 @@
 variable "project" {
   type    = string
-  default = "sample" # FIXME or create terraform.tfvars file
 }
 
 variable "region" {
   type    = string
-  default = "asia-northeast1"
 }
 
 variable "zone" {
   type    = string
-  default = "asia-northeast1-a"
 }
 
 variable "machine_type" {
   type    = string
-  default = "n1-standard-1"
 }
 
 variable "network_tags" {
   type    = list(string)
-  default = ["minecraft"]
+}
+
+variable "server_type" {
+  type    = string
+}
+
+variable "server_version" {
+  type    = string
 }
